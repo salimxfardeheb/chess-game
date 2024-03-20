2578f6279de13ce08da67c4f13bb7c7326e9985f
salimxfardeheb <salimsimou1331@gmail.com>
2024-03-20 03:34:50 +0100

    feat the main project and testing

--- data/classes/Piece.py
+++ data/classes/Piece.py
@@ -1,22 +1,21 @@
 import pygame
 
 class Piece:
-
-    def __init__(self, pos, color,board):
+    def __init__(self, pos, color, board):
         self.pos = pos
         self.x = pos[0]
-        self.y =  pos[1]
+        self.y = pos[1]
         self.color = color
         self.has_moved = False
 
-    def get_moves(self,board):
+    def get_moves(self, board):
         output = []
         for direction in self.get_possible_moves(board):
             for square in direction:
                 if square.occupying_piece is not None:
-                    if square.occupying_piece.color != self.color:
+                    if square.occupying_piece.color == self.color:
                         break
-                    else: 
+                    else:
                         output.append(square)
                         break
                 else:
@@ -32,30 +31,36 @@ class Piece:
     
     def move(self, board, square, force=False):
         for i in board.squares:
-            i.highlighted = False
+            i.highlight = False
         if square in self.get_valid_moves(board) or force:
             prev_square = board.get_square_from_pos(self.pos)
             self.pos, self.x, self.y = square.pos, square.x, square.y
-            prev_square.occupying_piece = self
+            prev_square.occupying_piece = None
+            square.occupying_piece = self
             board.selected_piece = None
             self.has_moved = True
-
-            if self.notation == '':
-                if self.y == 0 or self.y == '7':
-                    from data.classes.pieces.Queen import  Queen
-                    square.occupying_piece = Queen((self.x, self.y), self.color, board)
-
+            # Pawn promotion
+            if self.notation == ' ':
+                if self.y == 0 or self.y == 7:
+                    from data.classes.pieces.Queen import Queen
+                    square.occupying_piece = Queen(
+                        (self.x, self.y),
+                        self.color,
+                        board
+                    )
+            # Move rook if king castles
             if self.notation == 'K':
-                if prev_square - self.x == 2:
+                if prev_square.x - self.x == 2:
                     rook = board.get_piece_from_pos((0, self.y))
-                    rook.move(board, board.get_square_from_pos((3, self.y)),force = True)
-                elif  prev_square - self.x == -2:
+                    rook.move(board, board.get_square_from_pos((3, self.y)), force=True)
+                elif prev_square.x - self.x == -2:
                     rook = board.get_piece_from_pos((7, self.y))
-                    rook.move(board, board.get_square_from_pos((5, self.y)),force = True)
+                    rook.move(board, board.get_square_from_pos((5, self.y)), force=True)
             return True
         else:
             board.selected_piece = None
             return False
-        
+
+    # True for all pieces except pawn
     def attacking_squares(self, board):
         return self.get_moves(board)
--- data/classes/pieces/Rook.py
+++ data/classes/pieces/Rook.py
@@ -5,7 +5,7 @@ from data.classes.Piece import Piece
 class Rook(Piece):
     def __init__(self, pos, color, board):
         super().__init__(pos, color, board)
-        img_path = 'data/imgs/' + color[0] + '_rook.png'
+        img_path = 'data/images/' + color[0] + '_rook.png'
         self.img = pygame.image.load(img_path)
         self.img = pygame.transform.scale(self.img, (board.tile_width - 20, board.tile_height - 20))
         self.notation = 'R'
